5aa914dd216d01e1e9d665cc8505d4b629c1ab4a
Nikola Trubitsyn <nikola.trubitsyn@gmail.com>
2018-05-31 21:12:24 +0300

    Implement LocalDataSource

--- app/src/main/kotlin/io/github/trubitsyn/diary/api/LocalDataSource.kt
+++ app/src/main/kotlin/io/github/trubitsyn/diary/api/LocalDataSource.kt
@@ -5,6 +5,14 @@ import org.joda.time.LocalDate
 
 class LocalDataSource : DataSource {
     override fun getTasks(date: LocalDate): List<Task> {
-        return listOf()
+        return listOf(
+                Task("География", "повторение темы"),
+                Task("Биология", "записи в тетради"),
+                Task("Химия", "§ 16 в. 1, 2"),
+                Task("Математика (алгебра и начала анализа)", "повторить пройденное"),
+                Task("Математика (геометрия)", "№ 650"),
+                Task("Литература", "вопросы стр. 183-185"),
+                Task("Информатика", "конспект")
+        )
     }
 }
